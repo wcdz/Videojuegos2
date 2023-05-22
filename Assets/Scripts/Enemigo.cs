771cbef797b0307320d75179aedc140dd39eab09
William Chávez Díaz <chavezdiaz4@gmail.com>
2023-05-22 17:58:28 -0500

    First commit

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    public int saludEnemigo = 100;
+    public GameObject elemento;
+
+    public void Atacado(int damage){
+        saludEnemigo -= damage;
+        if(saludEnemigo <= 0){ Muere();}
+    }
+
+    void Muere(){
+        Instantiate(elemento, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
